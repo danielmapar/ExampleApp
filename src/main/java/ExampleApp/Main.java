4d15938deb03e40078a43be58214185ca59b1596
Daniel <danielmapar@gmail.com>
2019-02-08 18:21:36 -0500

    First commit

---
+++ src/main/java/ExampleApp/Main.java
@@ -0,0 +1,9 @@
+package main.java.ExampleApp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+
+}
---
+++ src/test/java/ExampleApp/MainTest.java
@@ -0,0 +1,19 @@
+package test.java.ExampleApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	}
+
+}
